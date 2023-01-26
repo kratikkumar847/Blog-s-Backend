4045ac9a413a96e31cf592142a39e52cedb5e5a9
Kratik Kumar <kratikkumar847@gmail.com>
2023-01-26 15:33:18 +0530

    adding post controllers

--- routes/post.route.js
+++ routes/post.route.js
@@ -5,4 +5,10 @@ const postController = require('../controllers/post.controller')
 // create a new post
 router.post('/newpost', postController.createPost);
 
+// create a new post
+router.get('/getposts', postController.createPost);
+
+// create a new post
+router.delete('/delete', postController.createPost);
+
 module.exports = router 
